f4987e6a6de43ce2eddf3e8c166f791adb5e822d
Daphne205 <126838439+Daphne205@users.noreply.github.com>
2023-10-13 11:04:44 +1300

    Firrrrrrreeeeeee
    switch switch side

--- Tech2023/Assets/Scripts/EnemyController.cs
+++ Tech2023/Assets/Scripts/EnemyController.cs
@@ -18,5 +18,15 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
       transform.position = Vector2.MoveTowards(transform.position, PandaPlayer.position, speed * Time.deltaTime);  
+      //calculates the horizontal direction and determines which way the fire needes to face
+      float horizontalDirection = Mathf.Sign(PandaPlayer.position.x - transform.position.x);
+
+if (horizontalDirection > 0.01)
+ // Flip the character to face right
+   transform.localScale = new Vector2(-5,5);
+ // Flip the character to face left
+   else if (horizontalDirection < -0.01)
+   transform.localScale  = new Vector2(5,5);  
     }
 }
+
